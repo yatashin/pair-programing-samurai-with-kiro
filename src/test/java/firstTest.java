13d4e567d468797c52684dbe8f24f55d74e51e2e
yatashin <s-yata@creationline.com>
2025-10-07 17:48:23 +0900

    setup truth

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class firstTest {
     @Test
     void firstFailed() {
 
-        assertEquals(1, 0);
+        assertEquals(1, 1);
+        assertThat(1).isEqualTo(0);
     }
 }
